a5085739d8d1d2431c32a803260e95c934e4052e
AbdifatahYasin1 <iskatag123@gmail.com>
2022-10-16 16:58:36 +0300

    Updated menu

---
+++ App.js
@@ -0,0 +1,21 @@
+
+//Create two varaible that holding hamburger button and Menu
+
+const hamburger = document.querySelector('.hamburger-btn');
+const navMenu = document.querySelector('.nav-menu');
+
+
+//Make event listeners
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+
+//looping for nav link
+
+document.querySelectorAll('.anchor').forEach((navLink) => navLink.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  }));
